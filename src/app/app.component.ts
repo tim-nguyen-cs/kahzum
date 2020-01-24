9d2bf36786dc067ed157924e0ea73ebd201b36ae
Tim <tim.nguyen.cs@gmail.com>
2020-01-24 00:41:29 -0800

    added charts and regression analysis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+import regression from 'regression';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +12,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'kahzum';
 
+  dataSet: number[] = [65, 59, 80, 81, 56, 55, 140];
+
+
+  dataPoints: number[][] = this.dataSet.reduce((rows, arrayValue, index) => (rows.push([index, arrayValue])) && rows, []);
+  result = regression.linear(this.dataPoints);
+  slope: number = this.result.equation[0];
+
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabels: Label[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+
+  barChartData: ChartDataSets[] = [
+    {
+      data: this.dataSet, label: '# of Customers'
+    },
+  ];
+
   openModal() {
     console.log("I got clicked!");
   }
+
 }
